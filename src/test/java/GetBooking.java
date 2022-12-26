2394359d29b7144cef8c6a9ee30ae5be18e1ec09
sgarine4 <sureshbabu.garine4@t-mobile.com>
2022-12-25 20:02:40 -0800

    first commit

---
+++ src/test/java/GetBooking.java
@@ -0,0 +1,21 @@
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class GetBooking {
+    public static void main(String[] args) {
+        RestAssured
+                .given()
+                .log()
+                .all()
+                .baseUri("https://restful-booker.herokuapp.com")
+                .basePath("booking/{id}")
+                .pathParam("id",1)
+                .contentType(ContentType.JSON)
+                .when()
+                .get()
+                .then()
+                .log()
+                .all()
+                .statusCode(200);
+    }
+}
